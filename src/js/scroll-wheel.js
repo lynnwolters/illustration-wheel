b696cde1f1187dd13f763065b3ac77e531be6105
Lynn Wolters <lynn.wolters27@gmail.com>
2023-06-16 14:51:55 +0200

    afronden

--- src/js/scroll-wheel.js
+++ src/js/scroll-wheel.js
@@ -2,47 +2,51 @@ import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 gsap.registerPlugin(ScrollTrigger)
 
-// let tl = gsap.timeline({
-//     scrollTrigger:{
-//         trigger: 'body',
-//         pin: true,
-//         scrub: 6,
-//     }
-// })
+let triggers = document.querySelectorAll('.js-trigger')
 
-// tl.to('.js-wheel', {rotation: 360})
-
-// ScrollTrigger.defaults({
-//     scroller: '.wheel'
-// })
-
-// gsap.to('.item-1', {
-//     scrollTrigger: '.item-1', 
-// })
-
-// gsap.to('.item-2', {
-//     scrollTrigger: '.item-2',  
-// })
-
-// gsap.to('.item-3', {
-//     scrollTrigger: '.item-3',  
-// })
-
-// gsap.to('.item-4', {
-//     scrollTrigger: '.item-4',  
-// })
-
-let items = gsap.utils.toArray('.item')
-
-gsap.to(items, {
-  xPercent: -100 * (items.length - 1),
-    scrollTrigger: {
-        trigger: 'body',
-        pin: true,
-        scrub: 1,
-        snap: 1 / (items.length - 1),
-    }
+triggers.forEach((item, index) => { 
+    let onTitle = document.querySelectorAll('.js-title')
+    let tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: item,
+            start: 'top top',
+            end: 'bottom top',
+            markers: false,
+            onEnter: () => {
+                tl
+                .to('.js-wheel', {rotation: `${45 * index}`, duration: 2,})
+                .to(onTitle, { opacity: 1 })
+            },
+            onEnterBack: () => {
+                tl
+                .to('.js-wheel', {rotation: `${45 * index}`, duration: 2})
+                .to(onTitle, { opacity: 1 })
+            },
+            onLeave: () => {
+                tl
+                .to(onTitle, { opacity: 0 })
+            },
+            onLeaveBack: () => {
+                tl
+                .to(onTitle, { opacity: 0 })
+            },
+        }
+    })
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 import './style.scss'
 import './styles/scroll-wheel.scss'
 import './js/scroll-wheel.js'
+
+
