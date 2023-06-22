bb07231dbe440578e4eab8ea07c3810eaab90e85
Lynn Wolters <lynn.wolters27@gmail.com>
2023-06-22 15:14:51 +0200

    afronden

--- src/js/scroll-wheel.js
+++ src/js/scroll-wheel.js
@@ -3,8 +3,9 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger'
 gsap.registerPlugin(ScrollTrigger)
 
 let triggers = document.querySelectorAll('.js-trigger')
+let cards = document.querySelectorAll('.js-card')
 
-triggers.forEach((item, index) => { 
+triggers.forEach((item, index) => {
     let tl = gsap.timeline({
         scrollTrigger: {
             trigger: item,
@@ -13,50 +14,65 @@ triggers.forEach((item, index) => {
             markers: false,
             onEnter: () => {
                 tl
-                .to('.js-wheel', {rotation: `${45 * index}`, duration: 2,})
-                .to(onTitle, { opacity: 1 })
-            },
-            onEnterBack: () => {
-                tl
-                .to('.js-wheel', {rotation: `${45 * index}`, duration: 2})
-                .to(onTitle, { opacity: 1 })
-            },
-            onLeave: () => {
-                tl
-                .to(onTitle, { opacity: 0 })
-            },
-            onLeaveBack: () => {
-                tl
-                .to(onTitle, { opacity: 0 })
-            },
-        }
-    })
+                // wheel
+                .to('.js-wheel', {rotation: `${-45 * index}`, duration: 2})
 
-    let tl2 = gsap.timeline({
-        scrollTrigger: {
-            trigger: item,
-            start: 'top top',
-            end: 'bottom top',
-            markers: false,
-            onEnter: () => {
-                tl2
-                .to('.js-title', { opacity: 1 })
+                // show title
+                .to(cards[index].querySelector('.js-title'), {opacity: 1}, '-=1')
+
+                // move title
+                .fromTo(cards[index].querySelector('.js-title'), {y: 50}, {y: -0}, '-=1')
+
+                // show image
+                .to(cards[index].querySelector('.js-image'), {opacity: 1}, '-=1')
+
+                // scale image
+                .to(cards[index].querySelector('.js-image'), {scale: 1.2}, '-=1')
             },
             onEnterBack: () => {
-                tl2
-                .to('.js-title', { opacity: 1 })
+                tl
+                // wheel
+                .to('.js-wheel', {rotation: `${-45 * index}`, duration: 2})
+
+                // show title
+                .to(cards[index].querySelector('.js-title'), {opacity: 1}, '-=1')
+
+                // move title
+                .fromTo(cards[index].querySelector('.js-title'), {y: 50}, {y: -0}, '-=1')
+
+                // show image
+                .to(cards[index].querySelector('.js-image'), {opacity: 1}, '-=1')
+
+                // scale image
+                .to(cards[index].querySelector('.js-image'), {scale: 1.2}, '-=1')
             },
             onLeave: () => {
-                tl2
-                .to('.js-title', { opacity: 0 })
+                tl
+                // hide title
+                .to(cards[index].querySelector('.js-title'), {opacity: 0})
+
+                // hide image
+                .to(cards[index].querySelector('.js-image'), {opacity: .5})
+
+                // scale back image
+                .to(cards[index].querySelector('.js-image'), {scale: 1}, '-=1')
             },
             onLeaveBack: () => {
-                tl2
-                .to('.js-title', { opacity: 0 })
+                tl
+                // hide title
+                .to(cards[index].querySelector('.js-title'), {opacity: 0})
+
+                // hide image
+                .to(cards[index].querySelector('.js-image'), {opacity: .5})
+
+                // scale back image
+                .to(cards[index].querySelector('.js-image'), {scale: 1}, '-=1')
             },
         }
     })
 })
+
+
 
 
 
