2026b9ec6c370fcdc1d6bc5bf7926cce0fb98d13
Lynn Wolters <lynn.wolters27@gmail.com>
2023-06-23 14:38:44 +0200

    afronden

--- src/js/logic/scroll-wheel.js
+++ src/js/logic/scroll-wheel.js
@@ -1,106 +1,53 @@
-// import { gsap } from 'gsap'
-// import { ScrollTrigger } from 'gsap/ScrollTrigger'
-// gsap.registerPlugin(ScrollTrigger)
-
-// let triggers = document.querySelectorAll('.js-trigger')
-// let cards = document.querySelectorAll('.js-card')
-
-// triggers.forEach((item, index) => {
-//     let tl = gsap.timeline({
-//         scrollTrigger: {
-//             trigger: item,
-//             start: 'top top',
-//             end: 'bottom top',
-//             markers: false,
-//             onEnter: () => {
-//                 tl
-//                 // wheel
-//                 .to('.js-wheel', {rotation: `${-45 * index}`, duration: 2})
-
-//                 // show title
-//                 .to(cards[index].querySelector('.js-title'), {opacity: 1}, '-=1')
-
-//                 // move title
-//                 .fromTo(cards[index].querySelector('.js-title'), {y: 50}, {y: -0}, '-=1')
-
-//                 // show image
-//                 .to(cards[index].querySelector('.js-image'), {opacity: 1}, '-=1')
-
-//                 // scale image
-//                 .to(cards[index].querySelector('.js-image'), {scale: 1.2}, '-=1')
-//             },
-//             onEnterBack: () => {
-//                 tl
-//                 // wheel
-//                 .to('.js-wheel', {rotation: `${-45 * index}`, duration: 2})
-
-//                 // show title
-//                 .to(cards[index].querySelector('.js-title'), {opacity: 1}, '-=1')
-
-//                 // move title
-//                 .fromTo(cards[index].querySelector('.js-title'), {y: 50}, {y: -0}, '-=1')
-
-//                 // show image
-//                 .to(cards[index].querySelector('.js-image'), {opacity: 1}, '-=1')
-
-//                 // scale image
-//                 .to(cards[index].querySelector('.js-image'), {scale: 1.2}, '-=1')
-//             },
-//             onLeave: () => {
-//                 tl
-//                 // hide title
-//                 .to(cards[index].querySelector('.js-title'), {opacity: 0})
-
-//                 // hide image
-//                 .to(cards[index].querySelector('.js-image'), {opacity: .5})
-
-//                 // scale back image
-//                 .to(cards[index].querySelector('.js-image'), {scale: 1}, '-=1')
-//             },
-//             onLeaveBack: () => {
-//                 tl
-//                 // hide title
-//                 .to(cards[index].querySelector('.js-title'), {opacity: 0})
-
-//                 // hide image
-//                 .to(cards[index].querySelector('.js-image'), {opacity: .5})
-
-//                 // scale back image
-//                 .to(cards[index].querySelector('.js-image'), {scale: 1}, '-=1')
-//             },
-//         }
-//     })
-// })
-
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { Flip } from 'gsap/Flip'
 gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(Flip)
 
 export class ScrollWheel {
     constructor() {
         this.triggers = document.querySelectorAll('.js-trigger')
         this.cards = document.querySelectorAll('.js-card')
+        this.button = document.querySelector('.js-continue')
         this.init()
     }
 
     init = () => {
         this.scroll()
+        this.bindEvents()
+    }
+
+    bindEvents = () => {
+        this.button.addEventListener('click', () => {
+            this.flip()
+        })
+    }
+
+    flip = () => {
+        const state = Flip.getState('.js-begin-state')
+        let beginState = document.querySelector('.js-begin-state')
+        beginState.classList.add('flip')
+        Flip.from(state, {
+            duration: 0.5, 
+            ease: 'power1.inOut'
+        })
     }
 
     scroll = () => {
         this.triggers.forEach((item, index) => {
             let title = this.cards[index].querySelector('.js-title')
             let image = this.cards[index].querySelector('.js-image')
+            let snap = gsap.utils.snap([100, 50, 500])
             let tl = gsap.timeline({
                 scrollTrigger: {
                     trigger: item,
                     start: 'top top',
                     end: 'bottom top',
-                    markers: false,
+                    markers: true,
                     onEnter: () => {
                         tl
                         // wheel
-                        .to('.js-wheel', {rotation: `${-45 * index}`, duration: 2})
+                        .to('.js-wheel', {rotation: `${-45 * index}`, duration: 2, snap: 65})
         
                         // show title
                         .to(title, {opacity: 1}, '-=1')
