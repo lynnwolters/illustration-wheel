ab02fdce6444d79f84897e11f1f2081b33fcc294
Lynn Wolters <lynn.wolters27@gmail.com>
2023-06-25 23:58:21 +0200

    afronden

--- src/js/logic/scroll-wheel.js
+++ src/js/logic/scroll-wheel.js
@@ -10,13 +10,25 @@ gsap.registerPlugin(ScrollToPlugin)
 export class ScrollWheel {
     constructor() {
         this.triggers = document.querySelectorAll('.js-trigger')
-        this.cards = document.querySelectorAll('.js-card')
+        if (!this.triggers) {
+            return false
+        }
         this.cardsWheel = document.querySelectorAll('.js-card-wheel')
+        this.cards = document.querySelectorAll('.js-card')
         this.init()
     }
 
     init = () => {
+        this.firstTrigger(0)
         this.scroll()
+    }
+
+    firstTrigger = (index) => {
+        let title = this.cards[index].querySelector('.js-title')
+        let tl = gsap.timeline()
+        tl
+        .to(title, {opacity: 1}, '+=1')
+        
     }
 
     scroll = () => {
@@ -29,59 +41,25 @@ export class ScrollWheel {
                     end: 'bottom top',
                     markers: true,
                     onEnter: () => {
+                        this.firstTrigger(index)
                         tl
                         .to(this.cardsWheel, {rotation: `${-45 * index}`, duration: 2})
-                        .to(title, {opacity: 1})
-                        .fromTo(title, {y: 50}, {y: -0}, '-=.5')
+                        .fromTo(title, {y: 30}, {y: -0}, '-=1')
                     },
                     onEnterBack: () => {
                         tl
                         .to(this.cardsWheel, {rotation: `${-45 * index}`, duration: 2})
                         .to(title, {opacity: 1})
-                        .fromTo(title, {y: 50}, {y: -0}, '-=.5')
+                        .fromTo(title, {y: 30}, {y: -0}, '-=1')
                     },
                     onLeave: () => {
-                        tl
-                        .to(title, {opacity: 0})
+                        tl.to(title, {opacity: 0})
                     },
                     onLeaveBack: () => {
-                        tl
-                        .to(title, {opacity: 0})
+                        tl.to(title, {opacity: 0})
                     },
                 }
             })
         })
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
